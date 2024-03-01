d5ac1501711e51760ebf4dac3a7cea4bd79ce50e
Mokshada Poudyal <64736265+Mokshada2001@users.noreply.github.com>
2024-03-01 19:32:05 +0545

    Update register_validation.js

--- register_validation.js
+++ register_validation.js
@@ -1,6 +1,4 @@
 const form = document.querySelector("form");
-// nField = form.querySelector(".name"),
-// nInput = nField.querySelector("input"),
 eField = form.querySelector(".email"),
 eInput = eField.querySelector("input"),
 pField = form.querySelector(".password"),
@@ -10,34 +8,20 @@ cpInput = cpField.querySelector("input"),
 
 form.onsubmit = (e)=>{
   e.preventDefault();
-  // (nInput.value == "") ? nField.classList.add("shake", "error") : checkUsername();
   (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
   (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
   (cpInput.value == "") ? cpField.classList.add("shake", "error") : checkPass1();
   
   setTimeout(()=>{ 
-    // nField.classList.remove("shake");
     eField.classList.remove("shake");
     pField.classList.remove("shake");
     cpField.classList.remove("shake");
   }, 500);
   
-  // nInput.onkeyup = ()=>{checkUsername();}
   eInput.onkeyup = ()=>{checkEmail();} 
   pInput.onkeyup = ()=>{checkPass();}
   cpInput.onkeyup = ()=>{checkPass1();} 
   
-  // function checkUsername(){ 
-  //   if(nInput.value == ""){ 
-  //     nField.classList.add("error");
-  //     nField.classList.remove("valid");
-  //   }
-  //   else{ 
-  //     nField.classList.remove("error");
-  //     nField.classList.add("valid");
-  //   }
-  // }
-
   function checkEmail(){ 
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
     if(!eInput.value.match(pattern)){ 
@@ -78,72 +62,4 @@ form.onsubmit = (e)=>{
     window.location.href = form.getAttribute("action"); 
   }
 }
-
-// const form = document.getElementById('form');
-// const username = document.getElementById('username');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-// const cnfpassword = document.getElementById('cnfpassword');
-
-// form.addEventListener('submit', e => {
-//   e.preventDefault();
-
-//   checkInputs();
-// });
-
-// function checkInputs() {
-//     // trim to remove the whitespaces
-//     const usernameValue = username.value.trim();
-//     const emailValue = email.value.trim();
-//     const passwordValue = password.value.trim();
-//     const cnfpasswordValue = cnfpassword.value.trim();
-
-//     if (usernameValue === '') {
-//         setErrorFor(username, 'Username cannot be blank');
-//     } else {
-//         setSuccessFor(username);
-//     }
-
-//     if (emailValue === '') {
-//         setErrorFor(email, 'Email cannot be blank');
-//     } else if (!isEmail(emailValue)) {
-//         setErrorFor(email, 'Not a valid email');
-//     } else {
-//         setSuccessFor(email);
-//     }
-
-//     if (passwordValue === '') {
-//         setErrorFor(password, 'Password cannot be blank');
-//     } else {
-//         setSuccessFor(password);
-//     }
-
-//     if (cnfpasswordValue === '') {
-//         setErrorFor(cnfpassword, 'Confirm Password cannot be blank');
-//     } else if (passwordValue !== cnfpasswordValue) {
-//         setErrorFor(cnfpassword, 'Passwords does not match');
-//     } else {
-//         setSuccessFor(cnfpassword);
-//     }
-//     }
-
-//     function setErrorFor(input, message) {
-//         const formControl = input.parentElement;
-//         const small = formControl.querySelector('small');
-//         formControl.className = 'form-control error';
-//         small.innerText = message;
-//     }
-
-//     function setSuccessFor(input) {
-//         const formControl = input.parentElement;
-//         formControl.className = 'form-control success';
-//     }
-
-//     function isEmail(email) {
-//     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-//         email
-//     );
-// }
-
-
 
